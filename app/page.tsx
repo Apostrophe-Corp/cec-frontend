b2a64b501432e7a502591f77b436e0554ce47f9c
Chiboyaz2 <njikagodwin212@gmail.com>
2025-03-18 23:04:02 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-underline text-2xl font-bold">
+        Hello word!!
+      </h1>
+    </div>
+  );
+}
